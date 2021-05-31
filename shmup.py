fa16f852c67489a9f1e4c9602492aef12194decc
Porosyatka <narbgo@yandex.ru>
2021-05-31 16:23:32 +0300

    sprait and control left-right

---
+++ shmup.py
@@ -0,0 +1,94 @@
+# Pygame шаблон - скелет для нового проекта Pygame
+import pygame
+import random
+#import os
+
+#from pygame.constants import K_RIGHT
+
+# настройка папки ассетов
+'''
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, 'img')
+player_img = pygame.image.load(os.path.join(img_folder, 'p1_jump.png'))
+'''
+
+WIDTH = 480  # ширина игрового окна
+HEIGHT = 600  # высота игрового окна
+FPS = 60  # частота кадров в секунду
+
+# Задаем цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# Создаем игру и окно
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Shmup!")
+clock = pygame.time.Clock()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(GREEN)
+        # self.image = player_img
+        # self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+
+# Цикл игры
+running = True
+while running:
+    # держим цикл на правильной скорости
+    clock.tick(FPS)
+    # ввод процесса (события)
+    for event in pygame.event.get():
+        # проверить закрытие окна
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Обновление
+    all_sprites.update()
+    # Рендеринг
+    screen.fill(BLUE)
+    all_sprites.draw(screen)
+    # после отрисовки всего, переворачиваем экран
+    pygame.display.flip()
+
+'''
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.speedx = -8
+            if event.key == pygame.KEYDOWN:
+                player.speedx = 8
+'''
+
+pygame.quit()
