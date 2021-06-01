8c819842702ec82a255d1c4142f9c529a52be7fa
Porosyatka <narbgo@yandex.ru>
2021-06-01 19:36:40 +0300

    readi game1

--- shmup.py
+++ shmup.py
@@ -1,18 +1,10 @@
 # Pygame шаблон - скелет для нового проекта Pygame
 import pygame
 import random
+from os import path
 
-#from pygame.event import set_allowed
-# import os
+img_dir = path.join(path.dirname(__file__), 'img')
 
-# from pygame.constants import K_RIGHT
-
-# настройка папки ассетов
-'''
-game_folder = os.path.dirname(__file__)
-img_folder = os.path.join(game_folder, 'img')
-player_img = pygame.image.load(os.path.join(img_folder, 'p1_jump.png'))
-'''
 
 WIDTH = 480  # ширина игрового окна
 HEIGHT = 600  # высота игрового окна
@@ -26,7 +18,7 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
-colors = [WHITE, RED, BLUE, YELLOW]
+# colors = [WHITE, RED, BLUE, YELLOW]
 
 
 # Создаем игру и окно
@@ -40,11 +32,12 @@ clock = pygame.time.Clock()
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 40))
-        self.image.fill(GREEN)
-        # self.image = player_img
-        # self.image.set_colorkey(BLACK)
+        self.image = pygame.transform.scale(player_img, (50, 38))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
+        self.radius = 20
+        # Рисую красный круг для определения границы столкновения
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
@@ -71,15 +64,40 @@ class Player(pygame.sprite.Sprite):
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((30, 40))
-        self.image.fill(RED)
+        self.image_orig = random.choice(meteor_images)
+        # self.image.set_colorkey(BLACK)
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
+
         self.rect = self.image.get_rect()
+        self.radius = int(self.rect.width * 0.85 / 2)
+        # Рисую красный круг для определения границы столкновения
+        # pygame.draw.circle(self.image, RED, self.rect.center, self.radius)
         self.rect.x = random.randrange(WIDTH - self.rect.width)
-        self.rect.y = random.randrange(-100, -40)
+        self.rect.y = random.randrange(-150, -100)
         self.speedy = random.randrange(1, 8)
         self.speedx = random.randrange(-3, 3)
+        self.rot = 0
+        self.rot_speed = random.randrange(-8, 8)
+        self.last_update = pygame.time.get_ticks()
+        # self.image = pygame.transform.rotate(self.image, self.rot_speed)
+
+    def rotate(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            new_image = pygame.transform.rotate(self.image_orig, self.rot)
+            old_center = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = old_center
+
+            # self.image = pygame.transform.rotate(self.image_orig, self.rot)
+            # Вращение спрайтов
 
     def update(self):
+        self.rotate()
         self.rect.x += self.speedx
         self.rect.y += self.speedy
         if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
@@ -91,8 +109,7 @@ class Mob(pygame.sprite.Sprite):
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 20))
-        self.image.fill(YELLOW)
+        self.image = bullet_img
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
@@ -105,6 +122,18 @@ class Bullet(pygame.sprite.Sprite):
             self.kill()
 
 
+# Загрузка всей игровой графики
+background = pygame.image.load(path.join(img_dir, 'starfield.png'))
+background_rect = background.get_rect()
+player_img = pygame.image.load(path.join(img_dir, 'playerShip1_orange.png'))
+# meteor_img = pygame.image.load(path.join(img_dir, 'meteorBrown_med1.png'))
+bullet_img = pygame.image.load(path.join(img_dir, 'laserRed16.png'))
+meteor_images = []
+meteor_list = ['meteorBrown_big1.png', 'meteorBrown_med1.png', 'meteorBrown_med1.png',
+               'meteorBrown_med3.png', 'meteorBrown_small1.png', 'meteorBrown_small2.png', 'meteorBrown_tiny1.png']
+for img in meteor_list:
+    meteor_images.append(pygame.image.load(path.join(img_dir, img)).convert())
+
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
@@ -114,7 +143,6 @@ for i in range(8):
     m = Mob()
     all_sprites.add(m)
     mobs.add(m)
-
 
 # Цикл игры
 running = True
@@ -140,24 +168,17 @@ while running:
         all_sprites.add(m)
         mobs.add(m)
 
-    hits = pygame.sprite.spritecollide(player, mobs, False)
+    hits = pygame.sprite.spritecollide(
+        player, mobs, False, pygame.sprite.collide_circle)
     if hits:
         running = False
 
     # Рендеринг
     screen.fill(BLACK)
+    screen.blit(background, background_rect)
     all_sprites.draw(screen)
     # после отрисовки всего, переворачиваем экран
     pygame.display.flip()
 
-
-'''
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player.speedx = -8
-            if event.key == pygame.KEYDOWN:
-                player.speedx = 8
-'''
 
 pygame.quit()
